deda055e8c9ab3eb94a90de54aaf9874de8fca8a
kollawat rupanya <micky2smart1@gmail.com>
2023-03-26 00:15:05 +0700

    add card

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -1,23 +1,40 @@
 import React from 'react';
 import './App.css';
 import Search from './components/search/search';
-
+import Card from 'react-bootstrap/Card';
 
 function App() {
   function handleSearchChange(searchData){
     console.log(searchData);
   }
+  
+  const current = new Date();
+  const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"];
+  const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const date = `${dayNames[current.getDay()]} ${current.getDate()}, ${monthNames[current.getMonth()]} ${current.getFullYear()}`;
+  const time = current.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+
 
 
   return (
+    <Card
+    className="card"
+    >
+    <Card.Body>
+      <Card.Title
+      className="title"
+      >{date}</Card.Title>
+      <Card.Subtitle
+      className="subtitle"
+      >{time}</Card.Subtitle>
     <div className="container">
-      <Search 
+      <Search
       onSearchChange={handleSearchChange}
       />
-    <card>
-      <h1>Weather App</h1>
-    </card>
     </div>
+    </Card.Body>
+    </Card>
   );
 }
 
