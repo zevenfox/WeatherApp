3d43668344760a14579e0dab9f79da8ed1476a4e
kollawat rupanya <micky2smart1@gmail.com>
2023-03-30 00:46:52 +0700

    decorate card

--- react-weather-app/src/components/current-weather/CurrentWeather.jsx
+++ react-weather-app/src/components/current-weather/CurrentWeather.jsx
@@ -1,22 +1,23 @@
 import './current-weather.css';
-import icon from '../../assets/weather-icon/weather1.png';
+import icon1 from '../../assets/weather-icon/weather1.png';
+import icon2 from '../../assets/weather-icon/weather2.png';
 
 
 function CurrentWeather(){
     return (
         <div className="weather">
             <div>
-            <img className="icon" src={icon} />
+            <img className="icon" src={icon2} />
             </div>
             <h1 className='city'>
-                Thailand
+                Bangkok
             </h1>
             <div>
                 <h1 className='temp'>
-                    30° C
+                    37° C
                 </h1>
                 <h2>
-                    Cloudy
+                    Sunny
                 </h2>
             </div>
         </div>
