a53e9f8f85cbdf84ef2bc1809fa567eec7ab501c
kollawat rupanya <micky2smart1@gmail.com>
2023-04-01 20:22:53 +0700

    modify search function

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -1,23 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Search from './components/search/Search';
 import Card from 'react-bootstrap/Card';
-import logo from './assets/logo-weather.png';
+// import logo from './assets/logo-weather.png';
 import CurrentWeather from './components/current-weather/CurrentWeather';
-
+import { WEATHER_API_URL, WEATHER_API_KEY } from './api';
 
 function App() {
-  function handleSearchChange(searchData){
-    console.log(searchData);
-  }
-  
-  const current = new Date();
-  const monthNames = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"];
-  const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  const date = `${dayNames[current.getDay()]} ${current.getDate()}, ${monthNames[current.getMonth()]} ${current.getFullYear()}`;
-  const time = current.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecastWeather, setForecastWeather] = useState(null);
 
+  function handleSearchChange(searchData) {
+    const [lat, lon] = searchData.value.split(' ');
+
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+    );
+
+    const forecastWeatherFetch = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+    );
+
+    Promise.all([currentWeatherFetch, forecastWeatherFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setForecastWeather({ city: searchData.label, ...forecastResponse });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  console.log(currentWeather);
+  console.log(forecastWeather);
+
+  const current = new Date();
+  const monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  const dayNames = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  const date = `${dayNames[current.getDay()]} ${current.getDate()}, ${
+    monthNames[current.getMonth()]
+  } ${current.getFullYear()}`;
+  const time = current.toLocaleString('en-US', {
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true,
+  });
 
   return (
     <Card
@@ -25,7 +75,7 @@ function App() {
     >
     <Card.Body>
     <div className='title-card'>
-      <Card.Title className="date"
+      <Card.Title className="date "
       >{date}
       <botton className = "bottonRight">
         +
@@ -46,7 +96,11 @@ function App() {
       onSearchChange={handleSearchChange}
       />
     </div>
-    <CurrentWeather />
+    <CurrentWeather 
+    data= {currentWeather}
+
+      
+    />
     {/* <div>
       <img 
       className="logo"
@@ -56,5 +110,6 @@ function App() {
     </Card>
   );
 }
+
 
 export default App;
--- react-weather-app/src/api.js
+++ react-weather-app/src/api.js
@@ -8,3 +8,5 @@ export const geoApiOptions = {
 
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = '37a24223c5a0c7694cf17d8422cb7e28';
